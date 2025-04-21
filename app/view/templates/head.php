732847dfeca67ded695de60c570a62d050a8de60
Kagrtavy <nesterivan97@gmail.com>
2025-04-21 21:42:56 +0300

    head in other file init

---
+++ app/view/templates/head.php
@@ -0,0 +1,7 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Main page - Azalea Pro</title>
+    <link href="<?= DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR . 'style.css' ?>" rel="stylesheet"/>
+    <link rel="icon" href="<?= DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR . 'azalea-logo.png' ?>" type="image/x-icon">
+</head>
