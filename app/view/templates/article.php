23e4b09b4a0730d69dbc9593918699290e3c3798
Kagrtavy <nesterivan97@gmail.com>
2025-04-21 21:42:46 +0300

    article init

---
+++ app/view/templates/article.php
@@ -0,0 +1,22 @@
+<article class="article-box">
+    <img src="/images/<?= $post['image'] ?>" alt="<?= $post['title'] ?>" class="article-photo">
+    <div class="article-wrap">
+        <header>
+            <div class="category">In <span><?= $post['category'] ?></span></div>
+            <h2 class="about-article">
+                <a href="../../../publication.php?id=<?= $post['id'] ?>">
+                    <?= $post['title'] ?>
+                </a>
+            </h2>
+            <div class="author">
+                <?= $post['author'] ?> -
+                <time datetime="<?= $post['created'] ?>">
+                    <?= date('F j, Y', strtotime($post['created'])) ?>
+                </time>
+            </div>
+        </header>
+        <div class="content">
+            <p><?= mb_strimwidth($post['content'], 0, 200, '...') ?></p>
+        </div>
+    </div>
+</article>
